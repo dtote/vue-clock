89e7050c42f660ff3dfed4d3924c48a16b1a0714
Nestor Torres <nestoreljudoka@gmail.com>
2021-03-11 12:20:36 +0000

    Subiendo proyecto

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+<div class="container">
+  <p>Creado por <strong>tote</strong></p>
+  <div class="clock-border">
+    <div class="clock-inner">
+      <div class="hour">{{hours}}</div>
+      <div class="dots">:</div>
+      <div class="min">{{minutes}}</div>
+      <div class="dots">:</div>
+      <div class="secs">{{seconds}}</div>
+      <div class="mode"></div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  },
+  methods: {
+    setTime () {
+      setInterval(() => {
+        const date = new Date()
+        this.hours = date.getHours()
+        this.minutes = date.getMinutes()
+        this.seconds = date.getSeconds()
+      }, 1000)
+    }
+  },
+  mounted () {
+    this.setTime()
+  }
+}
+
+</script>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    flex-direction: column;
+  }
+  .hour, .min, .secs {
+    font-size: 8em;
+    color:red;
+  }
+  strong {
+    color: blue;
+  }
+  p {
+    font-family: 'Lucida Sans', sans-serif;
+    font-size: 20px;
+  }
+  .clock-border {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 700px;
+    height: 200px;
+    background: linear-gradient(to right, grey, rgb(82, 76, 76));
+  }
+  .clock-inner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 600px;
+    height: 150px;
+    background: black;
+    border-radius: 20px;
+  }
+  .dots {
+    color: red;
+    font-size: 70px;
+  }
+</style>
